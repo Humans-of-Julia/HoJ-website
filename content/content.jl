887e224634063054350d2320a7b5829c3e24b711
Aaron <rapus95@gmail.com>
2021-02-10 19:35:22 +0100

    add proper self-description text & fix discord url

--- content/content.jl
+++ content/content.jl
@@ -27,11 +27,12 @@ user_to_name["azzaare"] = "Jean-François Baffier"
 about_hoj = Block(
     paragraphs(
     """
-    Human. Neither dog nor god.
+    We are a group of individuals who fell in love with Julia and pray to the gods of Lisp. We started to form a community around our favourite language which evolved into more than that. We’ve got regular code challenges, a GitHub Organization (https://github.com/Humans-of-Julia) which is meant to host projects for the community like the in-progress community-driven Julia bindings for the popular Grakn database client and we’re also a good destination if you’ve got questions anywhere from beginner to pro, need some valuable pointers to dig into it yourself or just seek the intellectual discourse about design and beauty (only Julia and Off-Topic of course). If you’d like to participate or are just curious, don’t hesitate to join us from anywhere around the world! We already have a good time zone coverage but we are - just as Julia itself - greedy to increase it even further!
     """,
+    """This community follows the Julia Community Guidelines and Standards!
+    """,
+    """Join our $(link("Discord","https://discord.gg/nPPZy4RYbP"))
     """
-    HoJ purpose. Join our $(link("Discord","https://discord.com/channels/762167454973296644/762206833192468480/762206959042166784"))
-    """,
     ),
     images()
 )
