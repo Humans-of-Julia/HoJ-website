c1814a8bcc49f1cea3cf1864055d3e6085d37aaf
Tom Kwong <tk3369@gmail.com>
2021-06-06 00:45:26 -0700

    Update content.jl

--- content/content.jl
+++ content/content.jl
@@ -32,7 +32,15 @@ user_to_name["varlad"] = "Chetan Vardhan"
 about_hoj = Block(
     paragraphs(
     """
-    We are a group of individuals who fell in love with Julia and pray to the gods of Lisp. We started to form a community around our favourite language which evolved into more than that. We’ve got regular code challenges, a $(link("GitHub Organization","https://github.com/Humans-of-Julia")) which is meant to host projects for the community like the in-progress community-driven Julia bindings for the popular Grakn database client and we’re also a good destination if you’ve got questions anywhere from beginner to pro, need some valuable pointers to dig into it yourself or just seek the intellectual discourse about design and beauty (only Julia and Off-Topic of course). If you’d like to participate or are just curious, don’t hesitate to join us from anywhere around the world! We already have a good time zone coverage but we are - just as Julia itself - greedy to increase it even further!
+    We are a group of individuals who fell in love with Julia and pray to the gods of Lisp. 
+    We formed a community around our favourite programming language and it has evolved to something bigger. 
+    We’ve got regular code challenges, a $(link("GitHub Organization","https://github.com/Humans-of-Julia")) 
+    which is meant to host community-driven projects such as TypeDB database client or our own Discord bot.
+    We are also a good destination if you’ve got questions anywhere 
+    from beginner to pro, need some valuable pointers or intellectual 
+    discourse about design and beauty (only Julia and Off-Topic of course). If you’d like to participate 
+    or are just curious, don’t hesitate to join us from anywhere around the world! We already have a 
+    good time zone coverage but we are - just as Julia itself - greedy to increase it even further!
     """,
     """
     This community follows the Julia Community Guidelines and Standards!
