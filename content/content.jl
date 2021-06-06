1593d111ad811bdfa4f302f7e87d29f663199021
Azzaare <jf@baffier.fr>
2021-06-06 17:04:51 +0900

    Fix linebreak

--- content/content.jl
+++ content/content.jl
@@ -32,15 +32,7 @@ user_to_name["varlad"] = "Chetan Vardhan"
 about_hoj = Block(
     paragraphs(
     """
-    We are a group of individuals who fell in love with Julia and pray to the gods of Lisp. 
-    We formed a community around our favourite programming language and it has evolved to something bigger. 
-    We’ve got regular code challenges, a $(link("GitHub Organization","https://github.com/Humans-of-Julia")) 
-    which is meant to host community-driven projects such as TypeDB database client or our own Discord bot.
-    We are also a good destination if you’ve got questions anywhere 
-    from beginner to pro, need some valuable pointers or intellectual 
-    discourse about design and beauty (only Julia and Off-Topic of course). If you’d like to participate 
-    or are just curious, don’t hesitate to join us from anywhere around the world! We already have a 
-    good time zone coverage but we are - just as Julia itself - greedy to increase it even further!
+    We are a group of individuals who fell in love with Julia and pray to the gods of Lisp. We formed a community around our favourite programming language and it has evolved to something bigger. We’ve got regular code challenges, a $(link("GitHub Organization","https://github.com/Humans-of-Julia")) which is meant to host community-driven projects such as TypeDB database client or our own Discord bot. We are also a good destination if you’ve got questions anywhere from beginner to pro, need some valuable pointers or intellectual discourse about design and beauty (only Julia and Off-Topic of course). If you’d like to participate or are just curious, don’t hesitate to join us from anywhere around the world! We already have a good time zone coverage but we are - just as Julia itself - greedy to increase it even further!
     """,
     """
     This community follows the Julia Community Guidelines and Standards!
